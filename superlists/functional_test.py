9d45337b308b7fe10903579b34e88e437d9bb81c
Ru Hong <ruhoo2004@gmail.com>
2017-05-12 16:19:28 +0800

    redirect POST, show all items in template

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -34,6 +34,7 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
